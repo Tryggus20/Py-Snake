c9c8b82d6e31ee834185258bc8c050de7a7b91c0
David Campbell <tryggus20@gmail.com>
2024-03-06 14:11:03 -0600

    added movement

--- snake.py
+++ snake.py
@@ -37,15 +37,45 @@ window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 # Game Fun
 snake = Tile(5*TILE_SIZE, 5*TILE_SIZE)
 food = Tile(10*TILE_SIZE, 10*TILE_SIZE)
+velocityX = 0
+velocityY = 0
+
+def change_direction(e): #e = event
+    # print(e) 
+    # print(e.keysym)  #prints out the keystroke
+    global velocityX, velocityY
+    if (e.keysym == "Up"):
+        velocityX = 0
+        velocityY = -1
+    elif (e.keysym == "Down"):
+        velocityX = 0
+        velocityY = 1
+    elif (e.keysym == "Left"):
+        velocityX = -1
+        velocityY = 0
+    elif (e.keysym == "Right"):
+        velocityX = 1
+        velocityY = 0
+
+def move():
+    global snake
+
+    snake.x += velocityX * TILE_SIZE
+    snake.y += velocityY * TILE_SIZE
 
 def draw():
     global snake
+    move()
 
+    #draw snake
     canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green")
 
+    #draw food
     canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill = "red")
+    
     window.after(100, draw) #Now with a whopping 60 frames per minute!
 
 draw()
 
+window.bind("<KeyRelease>", change_direction)
 window.mainloop()
